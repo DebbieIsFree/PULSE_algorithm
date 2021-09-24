7859ca482c7ed6ef67fc7f87dc4207bd741827f8
wldnjs980409 <wldnjs980409@naver.com>
2021-09-25 00:31:43 +0900

    Create 02_왕실의 나이트_풀이2.py

---
+++ Chpa04_implementation/02_왕실의 나이트_풀이2.py
@@ -0,0 +1,30 @@
+# 예외처리 추가 : '1a'와 같은 조합의 문자가 입력됐을 시 
+
+cur = input()
+
+if cur[0].isalpha() == False :
+    exit(0)
+
+
+y = ord(cur[0])     # ord() : 문자의 유니코드 반환
+x = int(cur[1])     # x (row), y(col)
+
+
+# 오른쪽 , 위 :  (+)
+# 왼쪽, 아래 : (-)
+dir = [(2,1),(2, -1),(-2,1),(-2,-1),(1,-2),(-1,-2),(1,2),(-1,2)]
+
+count = 0
+
+for i in dir:
+    xx = x + i[0]
+    yy = y + i[1]
+
+    if yy < 97 or yy > 104 or xx < 1 or xx > 8:
+        continue
+    count += 1
+
+print(count)
+
+
+
